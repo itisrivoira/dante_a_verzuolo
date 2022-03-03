94807c3682746164d826636f5f83aa398a266209
icobasco <enrico.allione@gmail.com>
2022-03-03 09:36:37 +0100

    Fix
    Rimosso sito bozza
    Img Play audio
    Allineamento titolo pagina

--- bozza_sito/aerdna/dante_a_verzuolo-main/dante_a_verzuolo-main/bozza_sito/js/script.js
+++
@@ -1,7 +0,0 @@
-const playAudio = () => {
-    var audio_player = document.getElementById("audio_player"); 
-    // if (audio_player.isPlaying())
-    //     audio_player.pause();
-    // else
-        audio_player.play();
-}
